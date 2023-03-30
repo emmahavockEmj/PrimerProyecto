49b024b197307432e37ab4c39370a9c1052ba401
Emmahavock} <moralesjimenez9890@gmail.com>
2023-03-29 18:46:46 -0600

    primer commit

---
+++ src/main/java/com/example/crud/controller/CrudController.java
@@ -0,0 +1,15 @@
+package com.example.crud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CrudController {
+	
+	//principal
+	@GetMapping({"/","/home","/index"})
+	public String home() {	
+		return "Home";
+	}
+
+}
