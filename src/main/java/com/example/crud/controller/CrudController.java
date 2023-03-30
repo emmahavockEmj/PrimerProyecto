46b7fd093d2754391b87d33aa6f082dc910f4124
Emmahavock} <moralesjimenez9890@gmail.com>
2023-03-29 20:52:46 -0600

    probando git

--- src/main/java/com/example/crud/controller/CrudController.java
+++ src/main/java/com/example/crud/controller/CrudController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CrudController {
 	
-	//principal
+	//principal probando git
 	@GetMapping({"/","/home","/index"})
 	public String home() {	
 		return "Home";
