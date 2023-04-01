acc7235e73b9ce36f5b44d546852d291c5b1fad1
Emmahavock} <moralesjimenez9890@gmail.com>
2023-03-31 20:48:52 -0600

    se agrega paque y clase de entitades para mapear la base

---
+++ src/main/java/com/example/crud/entity/Ciudad.java
@@ -0,0 +1,44 @@
+package com.example.crud.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="ciudades")
+public class Ciudad  implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY )
+	private Long id;
+	private String ciudad;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	@Override
+	public String toString() {
+		return "Ciudad [id=" + id + ", ciudad=" + ciudad + ", getId()=" + getId() + ", getCiudad()=" + getCiudad()
+				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+	
+	
+	
+	
+}
